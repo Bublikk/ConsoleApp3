daec9ce3f73e65ae463ddcbbd8a6bd68ea279e4b
Bublikk <ambartsum11@gmail.com>
2022-11-20 16:27:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿double a = Convert.ToDouble((string)Console.ReadLine());
+double b = Convert.ToDouble((string)Console.ReadLine());
+double e = Convert.ToDouble((string)Console.ReadLine());
+double c,x;
+
+double Sum(double x) => 2*Math.Sin(x)-Math.Atan(x);
+
+while (b-a > 2*e)
+{
+    c = (a + b) / 2;
+    if (Sum(a) * Sum(c) < 0)
+    {
+        b = c;
+    }
+    else
+    {
+        a = c;
+    }
+
+}
+x = (a + b) / 2;
+Console.WriteLine(Math.Round(x, 3));
+
+
